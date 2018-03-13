269706f871ccb412db5e7c1dbe8b27af5bf868f9
Anders <andershc92@hotmail.com>
2018-03-13 11:00:56 +0100

    Setup complete

---
+++ src/main/java/Entities/Address.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Entities;
+
+import java.io.Serializable;
+import java.util.Collection;
+import javax.persistence.Basic;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+/**
+ *
+ * @author AndersHC
+ */
+@Entity
+@Table(name = "Address")
+@NamedQueries({
+    @NamedQuery(name = "Address.findAll", query = "SELECT a FROM Address a")
+    , @NamedQuery(name = "Address.findByStreet", query = "SELECT a FROM Address a WHERE a.street = :street")
+    , @NamedQuery(name = "Address.findByNumber", query = "SELECT a FROM Address a WHERE a.number = :number")
+    , @NamedQuery(name = "Address.findByFloor", query = "SELECT a FROM Address a WHERE a.floor = :floor")})
+public class Address implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "street")
+    private String street;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 45)
+    @Column(name = "number")
+    private String number;
+    @Column(name = "floor")
+    private Integer floor;
+    @JoinColumn(name = "CityInfo_zipCode", referencedColumnName = "zipCode")
+    @ManyToOne(optional = false)
+    private CityInfo cityInfo;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "address")
+    private Collection<Person> personCollection;
+
+    public Address() {
+    }
+
+    public Address(String street) {
+        this.street = street;
+    }
+
+    public Address(String street, String number) {
+        this.street = street;
+        this.number = number;
+    }
+
+    public String getStreet() {
+        return street;
+    }
+
+    public void setStreet(String street) {
+        this.street = street;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+
+    public Integer getFloor() {
+        return floor;
+    }
+
+    public void setFloor(Integer floor) {
+        this.floor = floor;
+    }
+
+    public CityInfo getCityInfo() {
+        return cityInfo;
+    }
+
+    public void setCityInfo(CityInfo cityInfo) {
+        this.cityInfo = cityInfo;
+    }
+
+    public Collection<Person> getPersonCollection() {
+        return personCollection;
+    }
+
+    public void setPersonCollection(Collection<Person> personCollection) {
+        this.personCollection = personCollection;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (street != null ? street.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Address)) {
+            return false;
+        }
+        Address other = (Address) object;
+        if ((this.street == null && other.street != null) || (this.street != null && !this.street.equals(other.street))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Entities.Address[ street=" + street + " ]";
+    }
+    
+}
---
+++ src/main/java/Resource/DatCA2Resource.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Resource;
+
+import com.google.gson.Gson;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author AndersHC
+ */
+@Path("Data")
+public class DatCA2Resource {
+     Gson gson = new Gson();
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of DatCA2Resource
+     */
+    public DatCA2Resource() {
+    }
+
+    /**
+     * Retrieves representation of an instance of Entities.DatCA2Resource
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public String getJson() {
+        //TODO return proper representation object
+       return gson.toJson("hej");
+    }
+
+    /**
+     * PUT method for updating or creating an instance of DatCA2Resource
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    public void putJson(String content) {
+    }
+}
