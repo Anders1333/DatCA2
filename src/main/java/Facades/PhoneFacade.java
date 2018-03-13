fba3c1e8a9d4eadb5aaec90bc7a70a5610565b8d
Anders <andershc92@hotmail.com>
2018-03-13 13:14:01 +0100

    The gathering

--- src/main/java/Facades/PhoneFacade.java
+++ src/main/java/Facades/PhoneFacade.java
@@ -5,10 +5,32 @@
  */
 package Facades;
 
+import Entities.Phone;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 /**
  *
  * @author AndersHC
  */
 public class PhoneFacade {
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("DataCA2PU");
+
+    public static List<Phone> getAllPhoneNumbers(int id) {
+        EntityManager em = emf.createEntityManager();
+        Query query = em.createNamedQuery("Phone.findAll");
+        List<Phone> allPhoneNums = query.getResultList();
+        return allPhoneNums;
+    }
     
+    
+    public static Phone getPhoneByNumber(int id) {
+        EntityManager em = emf.createEntityManager();
+        Query query = em.createNamedQuery("Phone.findByNumber");
+        Phone p = (Phone) query.getSingleResult();
+        return p;
+    }
 }
