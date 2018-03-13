98159406f25fdfd4e7b2d0116e0d0d368965176e
DDomino <Andreasj54@gmail.com>
2018-03-13 13:04:54 +0100

    Andreas commit

--- src/main/java/Facades/PersonFacade.java
+++ src/main/java/Facades/PersonFacade.java
@@ -5,10 +5,31 @@
  */
 package Facades;
 
+import Entities.Person;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
 /**
  *
  * @author AndersHC
  */
 public class PersonFacade {
+    
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("DataCA2PU");
+    
+    public static Person getPersonFromId(int id){
+    EntityManager em = emf.createEntityManager();
+       Query q = em.createNamedQuery("Person.findById");
+       q.setParameter("id", id);
+       return (Person) q.getSingleResult(); 
+    }
+    public static List<Person> getAllPersons(){
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createNamedQuery("Person.findAll");
+        return q.getResultList();
+    }
     
 }
