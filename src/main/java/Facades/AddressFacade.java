ffafcd14c0405e5610de112609bb37455c4c470e
Zenzus <jonas.hein25@gmail.com>
2018-03-13 13:18:37 +0100

    Jonas commit

--- src/main/java/Facades/AddressFacade.java
+++ src/main/java/Facades/AddressFacade.java
@@ -5,16 +5,41 @@
  */
 package Facades;
 
+import Entities.Address;
 import Entities.Person;
+import static Entities.Person_.address;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
 
 /**
  *
  * @author AndersHC
  */
 public class AddressFacade {
-    
-    
-    
-    
-   
+
+    static EntityManagerFactory emf = Persistence.createEntityManagerFactory("DataCA2PU");
+
+    public static Address getAddress(int id) {
+        EntityManager em = emf.createEntityManager();
+        return em.find(Address.class, id);
+    }
+
+    public static List<Address> getAllAddress() {
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createNamedQuery("Address.findAll");
+        return q.getResultList();
+
+    }
+
+    public static List<Address> getAddressZipcode(int zipCode) {
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery("SELECT a FROM Address a WHERE Cityinfo_zipCode = :zipcode");
+        q.setParameter("zipcode", zipCode);
+        return q.getResultList();
+    }
+
 }
