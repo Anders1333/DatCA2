7f738364acca90d01ca448ef4bd1b187b81bef9e
Anders <andershc92@hotmail.com>
2018-03-14 11:30:01 +0100

    Day2begins

--- src/main/java/Facades/PersonFacade.java
+++ src/main/java/Facades/PersonFacade.java
@@ -6,6 +6,7 @@
 package Facades;
 
 import Entities.Person;
+import java.util.ArrayList;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -26,10 +27,70 @@ public class PersonFacade {
        q.setParameter("id", id);
        return (Person) q.getSingleResult(); 
     }
+    
     public static List<Person> getAllPersons(){
         EntityManager em = emf.createEntityManager();
         Query q = em.createNamedQuery("Person.findAll");
         return q.getResultList();
     }
     
+    public static Person getPersonFromPhone(int phonenumber){
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery("SELECT p.Person_id FROM Phone p WHERE p.number = : phonenumber");
+        q.setParameter("phonenumber", phonenumber);
+        int personId = (Integer) q.getSingleResult();
+        return (Person) getPersonFromId(personId);
+    }
+    
+    public static List<Person> getPersonsFromHobby(String hobby){
+        EntityManager em = emf.createEntityManager();
+        // First we get the hobby_id from the hobby name : 
+        Query getHobby = em.createQuery("SELECT h.hobbyId FROM Hobby h WHERE h.hobbyName = : hobby");
+        getHobby.setParameter("hobbyName", hobby);
+        int hobbyId = (Integer) getHobby.getSingleResult();
+        // Then we find the person_id from the Person_Has_Hobby list :
+        
+        Query q = em.createQuery("SELECT p.Person_id FROM Person_has_Hobby p WHERE p.Hobby_hobbyId = : hobbyId");
+        q.setParameter("hobbyId", hobbyId);
+        List<Integer> personIdList = q.getResultList();
+        // Now we create a list of persons from the Ids
+        List<Person> returnList = new ArrayList<>();
+        for (int i = 0; i < personIdList.size(); i++) {
+            int personToFind = personIdList.get(i);
+            Person personToAdd  = getPersonFromId(personToFind);
+            returnList.add(personToAdd);
+        }
+        return returnList;
+    }
+    
+    
+    
+    public static int getNumberOfPersonWithHobby(String hobby){
+           EntityManager em = emf.createEntityManager();
+         // First we get the hobby_id from the hobby name : 
+        Query getHobby = em.createQuery("SELECT h.hobbyId FROM Hobby h WHERE h.hobbyName = : hobby");
+        getHobby.setParameter("hobbyName", hobby);
+        int hobbyId = (Integer) getHobby.getSingleResult();
+        // Then we find the person_id from the Person_Has_Hobby list :
+        
+        Query q = em.createQuery("SELECT p.Person_id FROM Person_has_Hobby p WHERE p.Hobby_hobbyId = : hobbyId");
+        q.setParameter("hobbyId", hobbyId);
+        List<Integer> personIdList = q.getResultList();
+        int returnNumber = personIdList.size();
+        return returnNumber;
+    }
+    
+    
+    
+    public static List<Person> getPersonsFromCity(String cityName){
+        EntityManager em = emf.createEntityManager();
+        Query q = em.createQuery("SELECT c.zipCode FROM CityInfo c WHERE c.city = : city");
+        q.setParameter("city", cityName);
+        int zipCode = (Integer) q.getSingleResult();
+        // We now have the zipcode. We must get all persons with an address in the zipCode
+        Query x = em.createQuery("SELECT p FROM Person p WHERE p.address.cityInfo.zipCode = : zipCode");
+        x.setParameter("zipCode", zipCode);
+        List<Person> returnList = x.getResultList();
+        return returnList;
+    }
 }
